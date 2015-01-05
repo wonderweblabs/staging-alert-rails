8f32312098e522d29902ec119b2e34d964fbfd0e
Sascha Hillig <sascha@wonderweblabs.com>
2015-01-05 22:43:07 +0100

    add css

--- lib/staging_alert_rails/helper.rb
+++ lib/staging_alert_rails/helper.rb
@@ -2,8 +2,7 @@ module StagingAlertRails
   module Helper
 
     def staging_alert_bar
-      # render "../../app/views/staging_alert_rails/bar"
-      render "staging_alert_rails/bar"
+      render("staging_alert_rails/bar")
     end
 
   end
